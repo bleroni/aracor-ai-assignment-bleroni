ea6b015b70411f48c5b42eb8998d1dfd3e05c6a3
Bleron Limani <bleroni@gmail.com>
2025-02-20 15:02:42 +0100

    Calling both LLM models and switching between them

---
+++ src/models/model_manager.py
@@ -0,0 +1,30 @@
+from openai_config import openai_model
+from cohere_config import cohere_model
+
+
+class ModelManager:
+    def __init__(self):
+        self.client1 = openai_model
+        self.client2 = cohere_model
+        self.default_client = self.client1  # Initial default
+
+    def switch_client(self, new_default):
+        if new_default == "openai":
+            self.default_client = self.client1
+        elif new_default == "cohere":
+            self.default_client = self.client2
+        else:
+            raise ValueError("Unknown client")
+
+    def call_model(self):
+        resp = self.default_client.invoke("what's your name").content
+        print(f"Response from {self.default_client.model_name}: {resp}")
+
+
+if __name__ == "__main__":
+    model_manager = ModelManager()
+    model_manager.call_model()  # Default is OpenAI
+
+    # Switch to Cohere and call the model again
+    model_manager.switch_client("cohere")
+    model_manager.call_model()
---
+++ src/models/openai_config.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+import os
+from langchain.chat_models import init_chat_model
+from langchain.schema import SystemMessage, HumanMessage
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+openai_model = init_chat_model(
+  "gpt-4o",
+  model_provider="openai",
+  temperature=0,
+  openai_api_key=openai_api_key
+)
+
+# system_prompt = SystemMessage(content="You are a helpful assistant named Doctor Green.")
+
+# # Build the conversation history with previous messages
+# message_history = [
+#     system_prompt,
+#     HumanMessage(content="Hello, what's your name?")
+# ]
+
+# # Call the model with the history
+# response = openai_model.invoke(message_history)
+# print(response.content)
+
+# print("GPT-4o: " + gpt_4o_model.invoke("what's your name").content + "\n")
+# print("Model name: " + gpt_4o_model.model_name)
